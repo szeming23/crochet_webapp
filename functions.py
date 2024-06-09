ddc814937e8a99e1defeab2f089cab1e14a26be8
Sze Ming <koiszeming@u.nus.edu>
2024-06-10 00:27:33 +0800

    fix

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-img_dir = "/home/szeming/personal/crochet/crochet_imgs/"
+img_dir = "crochet_imgs/"
 
 def add_img(filename, caption):
     st.image(img_dir+filename, caption=caption, width=200)
